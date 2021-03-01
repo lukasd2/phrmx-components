5ac4d2c273505c973f1dd6325bab2287a132b711
Lukasz <beowfull@gmail.com>
2021-03-01 15:49:10 +0100

    Added max number of suggestions.Removed mocked examples, added storybook config disabling shortcuts.

---
+++ components/query-text/.storybook/manager.js
@@ -0,0 +1,15 @@
+import { addons } from '@web/storybook-prebuilt/addons';
+
+addons.setConfig({
+	isFullscreen: false,
+	showNav: true,
+	showPanel: true,
+	panelPosition: 'bottom',
+	sidebarAnimations: true,
+	enableShortcuts: false,
+	isToolshown: true,
+	theme: undefined,
+	selectedPanel: undefined,
+	initialActive: 'sidebar',
+	showRoots: false,
+});
