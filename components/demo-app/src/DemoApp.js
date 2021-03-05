b7a57fecc01daf719e7e82fd6d1acd870eca8e2c
Lukasz <beowfull@gmail.com>
2021-03-05 18:30:05 +0100

    demo app simple update to simplify UI. Added TrackEditor component as a dependency

--- components/demo-app/src/DemoApp.js
+++ components/demo-app/src/DemoApp.js
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from 'lit-element';
 import 'query-ui';
+import 'track-editor';
 
 export class DemoApp extends LitElement {
   static get properties() {
@@ -16,49 +17,23 @@ export class DemoApp extends LitElement {
         margin: 0 auto;
       }
 
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
-
-      main {
-        display: grid;
-        grid-template-columns: repeat(12, 1fr);
-        grid-column: 1 / 13;
-      }
-
       h1 {
         grid-column: 1/13;
         text-align: center;
-      }
-
-      .app-footer a {
-        margin-left: 5px;
       }
     `;
   }
 
   constructor() {
     super();
-    this.title = 'My app';
+    this.title = 'Demo APP: Components integration';
   }
 
   render() {
     return html`
-      <main>
-        <h1>${this.title}</h1>
-        <query-ui></query-ui>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <h1>${this.title}</h1>
+      <track-editor></track-editor>
+      <query-ui></query-ui>
     `;
   }
 }
