a78626cf29aa93e500c0b5dd4b7f1c47331fc0c2
Lukasz <beowfull@gmail.com>
2021-03-10 18:02:24 +0100

    track editor experiments

--- components/track-editor/src/TrackEditor.js
+++ components/track-editor/src/TrackEditor.js
@@ -7,6 +7,8 @@ export class TrackEditor extends LitElement {
     return {
       startTime: { type: Number },
       dragEnd: { type: Boolean },
+      timer: { type: Number },
+      actualTime: { type: Number },
     };
   }
 
@@ -16,20 +18,73 @@ export class TrackEditor extends LitElement {
 
   constructor() {
     super();
-    this.dropVideoArea = '';
-    this.dropMusicArea = '';
+    this.dropVideoArea;
+    this.dropMusicArea;
+    this.marker;
     this.startTime = 0;
     this.dragEnd = true;
+    this.timer = 0;
+    this.actualTime = 0;
+    this.interval;
   }
 
   connectedCallback() {
     super.connectedCallback();
     console.debug('DEBUG: TrackEditor successfuly added to the DOM');
+    window.addEventListener('mouseup', () => {
+      this.shadowRoot.removeEventListener(
+        'mousemove',
+        this._updateMarkerPosition
+      );
+    });
   }
 
   firstUpdated() {
     this.dropVideoArea = this.shadowRoot.querySelector('.video-track');
     this.dropMusicArea = this.shadowRoot.querySelector('.music-track');
+    this.marker = this.shadowRoot.getElementById('marker');
+    this.divideIntoTimeSegments();
+    /* marker.addEventListener('mousedown', ev => {
+      console.log(ev.target.offsetLeft);
+      const startX = ev.target.offsetLeft;
+      const mouseX = ev.clientX;
+      ev.target.style.transform = 'translateX(' + (mouseX - startX) + 'px)';
+    }); */
+
+    /*    this.timer = this.shadowRoot.getElementById('timer');
+    this.secondsCounter();
+    this.divideIntoTimeSegments(this.dropVideoArea); */
+  }
+
+  divideIntoTimeSegments() {
+    const dropVideoArea = this.dropVideoArea;
+    console.log(dropVideoArea);
+    console.log(getComputedStyle(dropVideoArea).width);
+    const segmentWidth = 500;
+    const totalWidth = dropVideoArea.offsetWidth;
+
+    let result = Math.floor(totalWidth / segmentWidth);
+    console.log('result', result);
+
+    const newElement = document.createElement('div');
+    newElement.classList.add('time-segment');
+    newElement.setAttribute('time-start', 0);
+    newElement.setAttribute('time-end', 5);
+    dropVideoArea.appendChild(newElement);
+
+    let timestart = 5.01;
+    let timeend = 10;
+    for (let i = 1; i < result; i++) {
+      let increment = 5;
+
+      const newElement = document.createElement('div');
+      newElement.classList.add('time-segment');
+      newElement.setAttribute('time-start', timestart.toFixed(2));
+      newElement.setAttribute('time-end', timeend);
+      timestart += increment;
+      timeend += increment;
+      dropVideoArea.appendChild(newElement);
+    }
   }
 
   updated(changedProperties) {
@@ -74,6 +129,9 @@ export class TrackEditor extends LitElement {
     if (ev.target.className === 'music-track') {
       this.dropMusicArea.classList.add('hoverWithDrag');
     }
+    if (ev.target.className === 'time-segment') {
+      ev.target.classList.add('hoverWithDrag');
+    }
   }
 
   _onDragOverMediaHandler(ev) {
@@ -111,7 +169,15 @@ export class TrackEditor extends LitElement {
     const img = document.createElement('img');
     img.src = dataSrc;
     element.appendChild(img);
-    ev.target.appendChild(element);
+    if (ev.target.className === 'time-segment') {
+      console.debug('time segment!');
+      ev.target.style.background = `url(${dataSrc}) repeat space`;
+      ev.target.style.backgroundPosition = `center `;
+      ev.target.style.backgroundRepeat = 'repeat space';
+      ev.target.classList.add('segment-active');
+    } else {
+      ev.target.appendChild(element);
+    }
   }
 
   extractDataFromDraggedElement(data) {
@@ -119,25 +185,100 @@ export class TrackEditor extends LitElement {
     return obj;
   }
 
+  secondsCounter = () => {
+    console.log(this.timer);
+
+    this.interval = window.setInterval(() => {
+      this.timer += 1;
+      this._updateMarkerPosition2();
+    }, 10);
+  };
+
+  _updateMarkerPosition = ev => {
+    const container = this.shadowRoot.querySelector('.timeline-container');
+    //console.log(this.marker);
+    //const startX = ev.target.offsetLeft;
+    const startX = container.getBoundingClientRect().left;
+    //problem offset starts from 8/9 px, it should be 0
+    //const startX = ev.target.offsetLeft;
+    const mouseX = ev.clientX;
+    /*     console.log('start', startX);
+    console.log('mouseX', mouseX); */
+    this.marker.style.transform = 'translateX(' + (mouseX - startX) + 'px)';
+    this.actualTime = parseInt(mouseX, 10);
+  };
+
+  _updateMarkerPosition2 = () => {
+    const container = this.shadowRoot.querySelector('.timeline-container');
+    //console.log(this.marker);
+    //console.log(ev.target.offsetLeft);
+    //const startX = ev.target.offsetLeft;
+    const startX = container.getBoundingClientRect().left;
+    //problem offset starts from 8/9 px, it should be 0
+    //const startX = ev.target.offsetLeft;
+    const mouseX = this.timer;
+    console.log('start', startX);
+    console.log('mouseX', mouseX);
+    this.marker.style.transform = 'translateX(' + (mouseX - startX) + 'px)';
+    this.actualTime = parseInt(mouseX, 10);
+  };
+
+  formatTime(time) {
+    console.log(time);
+    let houndreds = time;
+
+    let seconds = Math.floor((houndreds / 100) % 60);
+
+    return seconds + ':' + houndreds;
+  }
+
+  _handleMarkerPress(ev) {
+    console.log(this.dropVideoArea);
+    this.shadowRoot.addEventListener('mousemove', this._updateMarkerPosition);
+  }
+
+  _handleTimePlay(ev) {
+    console.debug('_handleTimePlay');
+    this.secondsCounter();
+  }
+
+  _handleTimeStop(ev) {
+    console.debug('_handleTimeStop');
+    clearInterval(this.interval);
+  }
+
   render() {
     return html`
+      <div class="timer-controls">
+        <input type="button" value="play" @click=${this._handleTimePlay} />
+        <input type="button" value="stop" @click=${this._handleTimeStop} />
+      </div>
+      <h1 id="timer">${this.timer}</h1>
+      <h2>${this.formatTime(this.actualTime)}</h2>
       <div class="track-editor">
-        <div
-          class="video-track"
-          @dragover="${this._onDragOverMediaHandler}"
-          @dragenter="${this._onDragEnterHandler}"
-          @dragleave="${this._onDragLeaveHandler}"
-          @drop="${this._onDropMediaHandler}"
-          @dragend=${this._onDragEnd}
-        ></div>
-        <div
-          class="music-track"
-          @dragover="${this._onDragOverMediaHandler}"
-          @dragenter="${this._onDragEnterHandler}"
-          @dragleave="${this._onDragLeaveHandler}"
-          @drop="${this._onDropMediaHandler}"
-          @dragend=${this._onDragEnd}
-        ></div>
+        <div class="timeline-container">
+          <div
+            id="marker"
+            class="timeline-marker"
+            @mousedown=${this._handleMarkerPress}
+          ></div>
+          <div
+            class="video-track"
+            @dragover="${this._onDragOverMediaHandler}"
+            @dragenter="${this._onDragEnterHandler}"
+            @dragleave="${this._onDragLeaveHandler}"
+            @drop="${this._onDropMediaHandler}"
+            @dragend=${this._onDragEnd}
+          ></div>
+          <div
+            class="music-track"
+            @dragover="${this._onDragOverMediaHandler}"
+            @dragenter="${this._onDragEnterHandler}"
+            @dragleave="${this._onDragLeaveHandler}"
+            @drop="${this._onDropMediaHandler}"
+            @dragend=${this._onDragEnd}
+          ></div>
+        </div>
       </div>
       <div style="margin-top: 50px;">
         <img
--- components/track-editor/src/styles/trackEditorStyles.js
+++ components/track-editor/src/styles/trackEditorStyles.js
@@ -7,7 +7,7 @@ export const trackEditorStyles = css`
   }
   .track-editor {
     width: 100%;
-    height: 30vh;
+    height: 33vh;
     background-color: #fff;
     border: 2px solid #1a2b42;
     box-sizing: border-box;
@@ -47,7 +47,7 @@ export const trackEditorStyles = css`
   .hoverWithDrag {
     box-sizing: border-box;
     border: 4px dotted salmon;
-    background-color: seashell;
+    background-color: tomato;
   }
   .timeline-element {
     width: 75px;
@@ -56,10 +56,42 @@ export const trackEditorStyles = css`
     margin: 0px 5px;
     display: flex;
   }
+
   .timeline-element img {
     position: relative;
     width: 100%;
     height: 100%;
     display: flex;
+  }
+
+  .timeline-container {
+    position: relative;
+    height: 100%;
+  }
+
+  .timeline-marker {
+    position: absolute;
+    top: -10px;
+    bottom: 0px;
+    width: 4px;
+    background: rgb(16, 85, 250) none repeat scroll 0% 0%;
+    pointer-events: all;
+    cursor: pointer;
+    z-index: 100;
+  }
+
+  .time-segment {
+    display: flex;
+    height: 60%;
+    flex: 1;
+    border: 2px solid aqua;
+    border-radius: 10px;
+  }
+  .time-segment.segment-active {
+    cursor: move;
+  }
+
+  .time-segment.segment-active:hover {
+    border: 1px solid red;
   }
 `;
